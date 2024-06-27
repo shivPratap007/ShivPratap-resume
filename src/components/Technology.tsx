0a69c5d97b4ed961cbd4a0ee16286769a488c57f
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-06-27 21:34:23 +0530

    fix: type issues

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -1,15 +1,14 @@
-import { FaReact } from "react-icons/fa";
-import { RiNextjsFill } from "react-icons/ri";
+import { Variants, motion } from "framer-motion";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { DiMongodb } from "react-icons/di";
-import { IoLogoJavascript } from "react-icons/io5";
-import { FaNodeJs } from "react-icons/fa";
-import { motion } from "framer-motion";
-import { SiTypescript } from "react-icons/si";
+import { FaNodeJs, FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { RiNextjsFill } from "react-icons/ri";
 
-
-const iconVariants = (duration) => ({
-  initial: { y: -10 },
+const iconVariants = (duration: number): Variants => ({
+  initial: {
+    y: -10,
+  },
   animate: {
     y: [10, -10],
     transition: {
@@ -22,12 +21,13 @@ const iconVariants = (duration) => ({
 });
 
 export default function Technology() {
+
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <motion.h2 whileInView={{opacity:1, y:0,}} initial={{opacity:0,y:-100}} transition={{duration:1.5}} className="my-20 text-center text-4xl">Technologies</motion.h2>
-      <motion.div whileInView={{opacity:1, x:0,}} initial={{opacity:0,x:-100}} transition={{duration:1.5}} className="flex flex-wrap items-center justify-center gap-4">
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <div className="flex flex-wrap items-center justify-center gap-4">
         <motion.div
-          variants={iconVariants(2.5)}
+          variants={iconVariants(1)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -35,7 +35,7 @@ export default function Technology() {
           <IoLogoJavascript className="text-7xl text-yellow-400" />
         </motion.div>
         <motion.div
-          variants={iconVariants(3)}
+          variants={iconVariants(1.5)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -43,7 +43,7 @@ export default function Technology() {
           <FaReact className="text-7xl text-cyan-400" />
         </motion.div>
         <motion.div
-          variants={iconVariants(3.5)}
+          variants={iconVariants(2)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -51,7 +51,7 @@ export default function Technology() {
           <RiNextjsFill className="text-7xl text-white-400" />
         </motion.div>
         <motion.div
-          variants={iconVariants(4)}
+          variants={iconVariants(2.5)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -59,7 +59,7 @@ export default function Technology() {
           <BiLogoPostgresql className="text-7xl text-sky-700" />
         </motion.div>
         <motion.div
-          variants={iconVariants(4.5)}
+          variants={iconVariants(3)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -67,14 +67,14 @@ export default function Technology() {
           <DiMongodb className="text-7xl text-green-400" />
         </motion.div>
         <motion.div
-          variants={iconVariants(5)}
+          variants={iconVariants(3.5)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <FaNodeJs className="text-7xl text-green-500" />
         </motion.div>
-      </motion.div>
+      </div>
     </div>
   );
 }
